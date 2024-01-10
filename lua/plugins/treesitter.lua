8e001e228703c17a45a212df898f217fa0a1cad5
Kronos-192081 <shiladityade.bwn2001@gmail.com>
2024-01-10 22:57:03 +0530

    added auto completions

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,24 +1,14 @@
 return {
-    {
-      "nvim-telescope/telescope-ui-select.nvim",
-    },
-    {
-      "nvim-telescope/telescope.nvim",
-      tag = "0.1.5",
-      dependencies = { "nvim-lua/plenary.nvim" },
-      config = function()
-        require("telescope").setup({
-          extensions = {
-            ["ui-select"] = {
-              require("telescope.themes").get_dropdown({}),
-            },
-          },
-        })
-        local builtin = require("telescope.builtin")
-        vim.keymap.set("n", "<C-p>", builtin.find_files, {})
-        vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-  
-        require("telescope").load_extension("ui-select")
-      end,
-    },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      local config = require("nvim-treesitter.configs")
+      config.setup({
+        auto_install = true,
+        highlight = { enable = true },
+        indent = { enable = true },
+      })
+    end
   }
+}
